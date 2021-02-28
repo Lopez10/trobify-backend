7d5666c4aeb9cafc60bfdfa979beadcea7d44317
Álvaro Hernández <34165099+aboredvaro@users.noreply.github.com>
2021-02-28 16:47:00 +0100

    Eliminamos los comentarios

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,3 @@ app.use(require('./routes/users'))
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
 });
-
-// Me cago en to tus muertos, tu quien eres?, eh, tu quien eres?
-// Toma te borro la linea xd

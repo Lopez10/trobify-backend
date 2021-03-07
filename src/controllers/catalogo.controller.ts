98dd78389e7cbfd99161ca091f0190758d84e894
Juan <ljuan7397@gmail.com>
2021-03-07 19:18:29 +0100

    arreglo mostrar catalogo

--- src/controllers/catalogo.controller.ts
+++ src/controllers/catalogo.controller.ts
@@ -4,7 +4,7 @@ import { Catalog } from '../interface/catalog.interface';
 
 export async function getCatalog(req: Request, res: Response): Promise<Response> {
 	const conn = await connect();
-	const catalogo = conn.query('SELECT * FROM Catalogo');
+	const catalogo = await conn.query('SELECT * FROM Catalogo');
 
-	return res.json(catalogo);
+	return res.json(catalogo[0]);
 }

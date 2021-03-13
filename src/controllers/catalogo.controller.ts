0523251ccd2a75381bfd123618fcc03af6c11624
Juan <ljuan7397@gmail.com>
2021-03-14 00:09:58 +0100

    mostrar imagenes de catalogo

--- src/controllers/catalogo.controller.ts
+++ src/controllers/catalogo.controller.ts
@@ -5,7 +5,7 @@ import { Catalog } from '../interface/catalog.interface';
 export async function getCatalog(req: Request, res: Response): Promise<Response> {
 	const conn = await connect();
 	const catalogo = await conn.query(
-		'SELECT * FROM Catalogo C, Inmueble I where I.id_inmueble = C.id_inmueble'
+		'SELECT * FROM Catalogo C, Inmueble I, Imagen Im where I.id_inmueble = C.id_inmueble and Im.catastro_id=I.catastro_id'
 	);
 
 	return res.json(catalogo[0]);

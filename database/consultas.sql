461d0e37907d531dc51c5e64074e4aca6e2d6da7
Karmelo1984 <carmelo.mc.1984@outlook.es>
2021-03-19 01:28:03 +0100

    Modificado getCatalog
    Se ha modificado la función getCatalog para mostrar más datos en el front

--- database/consultas.sql
+++ database/consultas.sql
@@ -20,4 +20,18 @@ select * from provincias;
 
 
 -- -------------------------------------------------------------------------------------
-	
+
+-- Consulta de la hiperfunción chupiguay
+
+SELECT inm.catastro_id as "catastro", cat.precio, inm.cant_Habitaciones as "nHab", inm.banos as "nBan", inm.cocina as "nCoc", inm.superficie as "area", ce.nombre as "certif", inm.breveDescripcion as "descrip", est.tipo as "estado", img.valor as "urlImg", pro.provincia, ubi.longitud, ubi.latitud, tpv.tipo as tpoViv 
+FROM catalogo cat, inmueble inm, ubicacion ubi, provincias pro, imagen img, CertificacionEnergetica ce, estado est, tipodevivienda tpv 
+WHERE (cat.catastro_id = inm.catastro_id 
+	and tpv.id = inm.id_vivienda 
+    and ubi.ubicacion_id = inm.ubicacion_id 
+    and ubi.prov = pro.provincia_id 
+    and img.catastro_id = inm.catastro_id 
+    and ce.id_certifEner = inm.id_certifEner 
+    and est.id = inm.id_estado 
+    and cat.id_modalidad = 1 
+    and pro.provincia_id = 46) 
+ORDER BY  cat.f_insercion DESC;

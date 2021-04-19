e9e5ebb7b0cdd806ca862360ee4ddf4e611e51f7
Karmelo1984 <carmelo.mc.1984@outlook.es>
2021-04-20 00:01:58 +0200

    Update inmueble.interface.ts

--- src/interface/inmueble.interface.ts
+++ src/interface/inmueble.interface.ts
@@ -1,1 +1,23 @@
-export interface Inmueble {}
+export interface Inmueble {
+	id_catastro: string;
+	tipoInmueble: string;
+	estadoInmueble: string;
+	energia: string;
+	imagen: string[];
+	superficie: number;
+	descripcion: string;
+	direccion: string;
+	longitud: number;
+	latitud: number;
+
+	tipoVivienda?: string;
+	cantHab?: number;
+	cantBanos?: number;
+	caracteristicas?: string[];
+	extras?: string[];
+
+	modalidad?: string;
+	submodalidad?: string;
+	precio?: number;
+	descuento?: number;
+}

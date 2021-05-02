981c54c34752002e9d185352a58fd4a647066f15
Karmelo1984 <carmelo.mc.1984@outlook.es>
2021-05-02 23:11:11 +0200

    Update catalogo.controller.ts

--- src/controllers/catalogo.controller.ts
+++ src/controllers/catalogo.controller.ts
@@ -44,11 +44,11 @@ export async function getCatalog(req: Request, res: Response): Promise<Response>
 	let idModalidad: number = Number(req.query.opt);
 
 	let select: string =
-		'inm.id_catastro, inm.superficie, inm.breveDescripcion, ubi.direccion, ubi.latitud, ubi.longitud, ubi.prov, cat.id_modalidad, cat.precio, cat.descuento, cat.id_usuario as propietario, car.nHab, car.nBano, car.nCocina, tpoI.tipoInmueble, tpoV.tipoVivienda, est.estadoInmueble, cer.certifEner';
+		'inm.id_catastro, inm.superficie, inm.breveDescripcion, ubi.direccion, ubi.latitud, ubi.longitud, ubi.prov, cat.id_modalidad, cat.precio, cat.descuento, cat.id_usuario as propietario, car.nHab, car.nBano, car.nCocina, tpoI.tipoInmueble, tpoV.tipoVivienda, est.estadoInmueble, cer.certifEner, img.valor as urlImg';
 	let from: String =
-		'inmueble inm, ubicacion ubi, catalogo cat, CaractIntrinsecas car, CertificacionEnergetica cer, EstadoInmueble est, TipoDeVivienda tpoV, TipoDeInmueble tpoI, caractintrinsecas intr';
+		'inmueble inm, ubicacion ubi, catalogo cat, CaractIntrinsecas car, CertificacionEnergetica cer, EstadoInmueble est, TipoDeVivienda tpoV, TipoDeInmueble tpoI, caractintrinsecas intr, imagen img';
 	let where: String =
-		'ubi.id_ubicacion = inm.id_ubicacion AND inm.id_catastro = cat.id_catastro AND inm.id_catastro = car.id_catastro AND inm.id_catastro = intr.id_catastro AND cer.id_certifEner = car.id_certifEner AND ubi.id_ubicacion = inm.id_ubicacion AND inm.id_estadoInmueble = est.id_estadoInmueble AND inm.id_tipoVivienda = tpoV.id_tipoVivienda AND inm.id_tipoInmueble = tpoI.id_tipoInmueble';
+		'ubi.id_ubicacion = inm.id_ubicacion AND inm.id_catastro = cat.id_catastro AND inm.id_catastro = car.id_catastro AND inm.id_imagen = img.id_imagen AND inm.id_catastro = intr.id_catastro AND cer.id_certifEner = car.id_certifEner AND ubi.id_ubicacion = inm.id_ubicacion AND inm.id_estadoInmueble = est.id_estadoInmueble AND inm.id_tipoVivienda = tpoV.id_tipoVivienda AND inm.id_tipoInmueble = tpoI.id_tipoInmueble';
 	where += ' AND cat.id_modalidad = ' + idModalidad;
 	where +=
 		'' + getSubconsultaModalidadProvinciaTipoinmueble(idModalidad, idTipoInmueble, idProvincia);
@@ -234,7 +234,7 @@ function getPrecio(minimo: number, maximo: number, aMrgn: string, margen: number
 	}
 	if (min < 0) min = 0;
 
-	console.log('AND cat.precio BETWEEN ' + min + ' AND ' + max);
+	//console.log('AND cat.precio BETWEEN ' + min + ' AND ' + max);
 
 	return ' AND cat.precio BETWEEN ' + min + ' AND ' + max;
 }

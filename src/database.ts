34cfd8dec75cfa48ee0136f4ed4e52402c374758
Juan <ljuan7397@gmail.com>
2021-05-20 18:34:51 +0200

    cambio pool

--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,13 @@
-import {createPool} from 'mysql2/promise';
+import { createPool } from 'mysql2/promise';
 
 export async function connect() {
-    const connection = await createPool({
-        host:'localhost',
-        user:'root',
-        password: 'rootroot',
-        database: 'Trobify',
-        port: 3306,
-        connectionLimit: 10
-    });
-     return connection;
+	const connection = await createPool({
+		host: 'localhost',
+		user: 'root',
+		password: 'rootroot',
+		database: 'Trobify',
+		port: 3306,
+		connectionLimit: 100,
+	});
+	return connection;
 }
-

e878677cfa5c6fceb89b151e4881e34e5ed18663
Javier García <jvrgrchrmsll@gmail.com>
2021-06-06 00:25:16 +0200

    Brahma (falta por ordenar y cambiar nombre)

--- src/controllers/ObjetosDeIntercambio/Builder/Brahma.ts
+++ src/controllers/ObjetosDeIntercambio/Builder/Brahma.ts
@@ -5,13 +5,22 @@ import { Contiene } from '../../BaseDeDatos/Contiene';
 import { Imagen } from '../../BaseDeDatos/Imagen';
 import { Inmueble } from '../../BaseDeDatos/inmueble';
 import { Catalogo } from '../../BaseDeDatos/Catalogo';
+import { DatosCatastro } from '../../BaseDeDatos/DatosCatastro';
 import { IntercambioInmueble } from "../IntercambioInmueble";
+import { VistaListado } from './VistaListado';
 
 export class Registro extends IntercambioInmueble{
     protected objetoDeIntercambio: DatosInmueble;
 
+    numberFromString(lista: string[]){
+        let pic2: number[];
+        for( let i = 0; i < lista.length; i++){
+            pic2[i] = parseInt(lista[i]);
+        }
+        return pic2;
+    }
 
-    registrar(req: Request) {
+    async registrar(req: Request) {
         let pic: string[] = String(req.query.imagen).split(',');
         let url: string[] = String(req.query.url).split(',');
         let caractSec: string[] = String(req.query.caracteristica).split(',');
@@ -19,58 +28,23 @@ export class Registro extends IntercambioInmueble{
         let precio: string[] = String(req.query.precio).split(',');
         let descuento: string[] = String(req.query.descuento).split(',');
         let publicado: string[] = String(req.query.publicado).split(',');
-        
-
-        let pic2: number[];
-        let caractSec2: number[];
-        let modal2: number[];
-        let precio2: number[];
-        let descuento2: number[];
-        let publicado2: number[];
 
         const fecha: Date = new Date();
         const hoy: string = '' + fecha.getFullYear() + '-' + fecha.getMonth() + '-' + fecha.getDay();
 
-        for( let i = 0; i < pic.length; i++){
-            pic2[i] = parseInt(pic[i]);
-        }
-
-        for( let i = 0; i < pic.length; i++){
-            caractSec2[i] = parseInt(caractSec[i]);
-        }
-
-        for( let i = 0; i < pic.length; i++){
-            modal2[i] = parseInt(modal[i]);
-        }
-
-        for( let i = 0; i < pic.length; i++){
-            precio2[i] = parseInt(precio[i]);
-        }
-
-        for( let i = 0; i < pic.length; i++){
-            descuento2[i] = parseInt(descuento[i]);
-        }
-
-        for( let i = 0; i < pic.length; i++){
-            publicado2[i] = parseInt(publicado[i]);
-        }
-
         let casa: Inmueble = new Inmueble(String(req.query.catastro), String(req.query.descripcion), Number(req.query.tipoInmueble), Number(req.query.estadoInmueble), Number(req.query.tipoVivienda), Number(req.query.imagen));
-        let imagen = new Imagen(pic2, String(req.query.catastro), url);
-        let contain = new Contiene(String(req.query.catastro), caractSec2);
-        let catalo = new Catalogo(String(req.query.catastro), modal2, precio2, descuento2, hoy, Number(req.query.propietario), publicado2);
+        let imagen = new Imagen(this.numberFromString(pic), String(req.query.catastro), url);
+        let contain = new Contiene(String(req.query.catastro), this.numberFromString(caractSec));
+        let catalo = new Catalogo(String(req.query.catastro), this.numberFromString(modal), this.numberFromString(precio), this.numberFromString(descuento), hoy, Number(req.query.propietario), this.numberFromString(publicado));
         let caractInt = new CaracteristicasIntrinsecas(String(req.query.catastro), Number(req.query.nBano), Number(req.query.nCocina), Number(req.query.nHab), Number(req.query.id_certifEner));
-        
-        casa.insertDatos();
-        imagen.insertDatos();
-        contain.insertDatos();
-        catalo.insertDatos();
-        caractInt.insertDatos();
+        let datos = new DatosCatastro(String(req.query.catastro), String(req.query.direccion), String(req.query.localidad), Number(req.query.codPostal), Number(req.query.provincia), Number(req.query.superficie), Number(req.query.latitud), Number(req.query.longitud));
+
+        await casa.insertDatos();
+        await imagen.insertDatos();
+        await contain.insertDatos();
+        await catalo.insertDatos();
+        await caractInt.insertDatos();
+        await datos.insertDatos();
     }
-	private id_catastro: string;
-	private nBano: number;
-	private nCocina: number;
-	private nHab: number;
-	private id_certifEner: number;
 
 }
